a5513685b7417515f46cb009aab8e35d4f7ff018
biwenKey <2753081006@qq.com>
2017-12-05 22:55:12 +0800

    第一版修改

--- day02/list.py
+++ day02/list.py
@@ -8,3 +8,14 @@ if names[0] == "ZhangYang":
     print("True")
 else:
     print("False")
+
+
+
+while True:
+    salary = input("请输入你的金额:")
+    if salary.isdigit():
+        salary = int(salary)
+        exit()
+    else:
+        print("你输入的不是数字")
+
--- day02/test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-while True:
-    salary = input("请输入你的金额:")
-    if salary.isdigit():
-        salary = int(salary)
-        exit()
-    else:
-        print("你输入的不是数字")
