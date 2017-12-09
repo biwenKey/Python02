e6cf8762d4b2338cac70130c07ed7666b79b3cd4
biwenKey <2753081006@qq.com>
2017-12-09 19:00:13 +0800

    test01

---
+++ day02/test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import getpass
+name = input("Please input your name:")
+password  = getpass.getpass("Please input your password:")
+print(name,password)
